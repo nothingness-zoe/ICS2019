8271f847b940c388964b620d363d916a102656c6
tracer-ics2019 <tracer@njuics.org>
2019-09-15 19:28:52 +0800

    >  compile 181220079 zoechou Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  19:28:52 up  3:42,  1 user,  load average: 0.07, 0.04, 0.00 513be81e89cfed80f0730829bffa4dfe6a36db1

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -48,7 +48,7 @@ static struct {
   { "help", "Display informations about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
-  { "si [N]","Suspend the execution of the program after stepping through N instructions. When N is not given, the default is 1", cmd_si }, 
+  { "si","Suspend the execution of the program after stepping through N instructions. When N is not given, the default is 1", cmd_si }, 
 
   /* TODO: Add more commands */
 
