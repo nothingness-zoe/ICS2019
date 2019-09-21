3a5768a9a789ca2771f34f4bc1a0b261b05e55b1
181220079-Zhou Yawen <181220079@smail.nju.edu.cn>
2019-09-21 22:39:54 +0800

    fix bug for expr()

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -7,7 +7,7 @@
 #include <regex.h>
 
 enum {
-  TK_NOTYPE = 256, TK_EQ
+  TK_NOTYPE = 256, TK_EQ = 1, TK_TEN = 10
 
   /* TODO: Add more token types */
 
@@ -24,7 +24,41 @@ static struct rule {
 
   {" +", TK_NOTYPE},    // spaces
   {"\\+", '+'},         // plus
-  {"==", TK_EQ}         // equal
+  {"\\-", '-'},			// minus
+  {"\\*", '*'},			// multiply
+  {"\\/", '/'},			// divide
+  {"\\(", '('},			// left parenthesis
+  {"\\)", ')'},			// right parenthesis
+  {"==", TK_EQ},        // equal
+  {"[0-9]+", TK_TEN},   // dec
+};
+
+#define NR_REGEX (sizeof(rules) / sizeof(rules[0]) )
+
+static regex_t re[NR_REGEX] = {};
+
+/* Rules are used for many times.
+ * Therefore we compile them only once before any usage.
+ */
+void init_regex() {
+  int i;
+  char error_msg[128];
+  int ret;
+
+  for (i = 0; i < NR_REGEX; i ++) {
+    ret = regcomp(&re[i], rules[i].regex, REG_EXTENDED);
+    if (ret != 0) {
+      regerror(ret, &re[i], error_msg, 128);
+      panic("regex compilation failed: %s\n%s", error_msg, rules[i].regex);
+    }
+  }
+}
+
+typedef struct token {
+  int type;
+  char str[32];
+} Token;
+
 };
 
 #define NR_REGEX (sizeof(rules) / sizeof(rules[0]) )
@@ -78,11 +112,40 @@ static bool make_token(char *e) {
          * to record the token in the array `tokens'. For certain types
          * of tokens, some extra actions should be performed.
          */
+        
+        if (rules[i].token_type == TK_NOTYPE) continue; 
 
         switch (rules[i].token_type) {
-          default: TODO();
+          case '+':{
+            tokens[nr_token].type = rules[i].token_type;
+            strncpy(tokens[nr_token].str, substr_start, substr_len);
+          } break;
+          case '-':{
+            tokens[nr_token].type = rules[i].token_type;
+            strncpy(tokens[nr_token].str, substr_start, substr_len);
+          } break;
+          case '*':{
+            tokens[nr_token].type = rules[i].token_type;
+            strncpy(tokens[nr_token].str, substr_start, substr_len);
+          } break;
+          case '/':{
+            tokens[nr_token].type = rules[i].token_type;
+            strncpy(tokens[nr_token].str, substr_start, substr_len);
+          } break;
+          case '(':{
+            tokens[nr_token].type = rules[i].token_type;
+            strncpy(tokens[nr_token].str, substr_start, substr_len);
+          } break;
+          case ')':{
+            tokens[nr_token].type = rules[i].token_type;
+            strncpy(tokens[nr_token].str, substr_start, substr_len);
+          } break;
+          case TK_TEN:{tokens[nr_token].type = rules[i].token_type;
+            strncpy(tokens[nr_token].str, substr_start, substr_len);
+          } break;
+          default: assert(0);
         }
-
+        nr_token++;
         break;
       }
     }
@@ -96,6 +159,69 @@ static bool make_token(char *e) {
   return true;
 }
 
+bool check_parentheses(int p, int q) {
+  int left = 1;
+  int right = 1;
+  if (!(tokens[p].type == '(' && tokens[q].type == ')')) return false;
+  p++;
+  for (; p < q; p++) {
+    if (tokens[p].type == '(') left += 1;
+    else if (tokens[p].type == ')') right +=1;
+    
+    if (right > left+1) assert(0); // bad expression
+    else if (right == left+1) return false; 
+    /* the leftmost "(" and the rightmost ")" are not matched */
+  }
+  
+  if (left != right) return false;
+
+  return true;
+}
+
+uint32_t find_main_op(int p, int q) {
+  int op = p;
+  int left = 0;
+  int right = 0;
+  for (int i = p; i < q; i++) {
+    if (tokens[i].type == '(') left++;
+    else if (tokens[i].type == ')') right++;
+
+    if (left == right) {
+      if (tokens[i].type == TK_TEN || tokens[i].type == TK_NOTYPE) continue;
+      if (tokens[i].type == '+' || tokens[i].type == '-') op = i;
+      if (tokens[i].type == '*' || tokens[i].type == '/') {
+        if (tokens[op].type == '+' || tokens[op].type == '-') continue;
+        else op = i;
+      }
+    }
+  }
+}
+
+uint32_t eval(int p, int q) {
+  if (p > q) assert(0);
+  else if (p == q) {
+    int number = 0;
+    sscanf(tokens[p].str, "%d", &number);
+    return number;
+  }
+  else if (check_parentheses(p, q) == true) return eval(p+1, q-1);
+  else {
+    int op;
+    op = find_main_op(p, q);
+    uint32_t val1, val2;
+    val1 = eval(p, op-1);
+    val2 = eval(op+1, q);
+
+    switch(tokens[op].type) {
+      case '+': return val1+val2; break;
+      case '-': return val1-val2; break;
+      case '*': return val1*val2; break;
+      case '/': return val1/val2; break;
+      default: assert(0);
+    }
+  }
+}
+
 uint32_t expr(char *e, bool *success) {
   if (!make_token(e)) {
     *success = false;
@@ -103,7 +229,7 @@ uint32_t expr(char *e, bool *success) {
   }
 
   /* TODO: Insert codes to evaluate the expression. */
-  TODO();
-
+  
+  eval(0, 31);
   return 0;
 }
