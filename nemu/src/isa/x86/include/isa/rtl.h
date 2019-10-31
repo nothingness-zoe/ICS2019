535ab3c218e6832075df52939ada795fe075b326
181220079-Zhou Yawen <181220079@smail.nju.edu.cn>
2019-10-31 19:54:07 +0800

    fix bug for rtl_update_SF()

--- nemu/src/isa/x86/include/isa/rtl.h
+++ nemu/src/isa/x86/include/isa/rtl.h
@@ -84,7 +84,8 @@ static inline void rtl_update_ZF(const rtlreg_t* result, int width) {
 
 static inline void rtl_update_SF(const rtlreg_t* result, int width) {
   // eflags.SF <- is_sign(result[width * 8 - 1 .. 0])
-  cpu.eflags.SF = rtl_shr(result, width*8-1);
+  rtl_shri(&t0, result, width*8-1);
+  rtl_set_SF(&t0);
 }
 
 static inline void rtl_update_ZFSF(const rtlreg_t* result, int width) {
