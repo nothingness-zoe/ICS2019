1e939e11e2272af43342a862a2295fe6aac2e869
181220079-Zhou Yawen <181220079@smail.nju.edu.cn>
2019-10-31 19:49:06 +0800

    fix bug for rtl_push() and rtl_pop()

--- nemu/src/isa/x86/include/isa/rtl.h
+++ nemu/src/isa/x86/include/isa/rtl.h
@@ -25,16 +25,16 @@ static inline void rtl_sr(int r, const rtlreg_t* src1, int width) {
 
 static inline void rtl_push(const rtlreg_t* src1) {
   // esp <- esp - 4
-  cpu.esp -= 4;
+  rtl_subi(&cpu.esp, &cpu.esp, 4);
   // M[esp] <- src1
-  operand_write(&cpu.esp, src1);
+  rtl_sm(&cpu.esp, src1, 4);
 }
 
 static inline void rtl_pop(rtlreg_t* dest) {
   // dest <- M[esp]
   rtl_lm(dest, &cpu.esp, 4);
   // esp <- esp + 4
-  cpu.esp += 4;
+  rtl_addi(&cpu.esp, &cpu.esp, 4);
 }
 
 static inline void rtl_is_sub_overflow(rtlreg_t* dest,
