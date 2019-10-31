421f8b5ab7a09a5a96236599039f374b37910369
tracer-ics2019 <tracer@njuics.org>
2019-10-31 18:23:13 +0800

    >  compile 181220079 zoechou Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  18:23:13 up  4:11,  1 user,  load average: 0.57, 0.34, 0.20 212c81736a6465c64ba6c5e14de7ea46ca8b80f4

--- nemu/src/isa/x86/exec/all-instr.h
+++ nemu/src/isa/x86/exec/all-instr.h
@@ -7,11 +7,13 @@ make_EHelper(operand_size);
 make_EHelper(inv);
 make_EHelper(nemu_trap);
 
-// control.c
+make_EHelper(call);
+make_EHelper(push);
+/*// control.c
 make_EHelper(jmp);
 make_EHelper(jcc);
 make_EHelper(jmp_rm);
-make_EHelper(call);
+
 make_EHelper(ret);
 make_EHelper(ret_imm);
-make_EHelper(call_rm);
+make_EHelper(call_rm);*/
