64d57185c989d9adcf24493b208213ecd1cf4c28
tracer-ics2019 <tracer@njuics.org>
2019-11-01 11:46:05 +0800

    >  compile 181220079 zoechou Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  11:46:05 up  6:54,  1 user,  load average: 0.48, 0.51, 0.55 b90a7d514dbcd5f3b3bac89a25c9cbdb501d1bee

--- nemu/include/rtl/rtl.h
+++ nemu/include/rtl/rtl.h
@@ -137,7 +137,7 @@ static inline void rtl_not(rtlreg_t *dest, const rtlreg_t* src1) {
 
 static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- signext(src1[(width * 8 - 1) .. 0])
-  printf("src1: %#x\n", *src1);
+  printf("src1: %#x\n width: %d\n", *src1, width);
 
   /*switch(width) {
     case 4: *dest = *src1;
