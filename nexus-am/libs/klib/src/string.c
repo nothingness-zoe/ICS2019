31cf7a12ad02b69ab057e4adb99095cb2974bc5c
181220079-Zhou Yawen <181220079@smail.nju.edu.cn>
2019-11-02 23:04:51 +0800

    complete klib/src/string.c

---
+++ nexus-am/libs/klib/src/string.c
@@ -0,0 +1,125 @@
+#include "klib.h"
+
+#if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
+
+size_t strlen(const char *s) {
+  size_t len = 0;
+  while (*s) {
+    len++;
+    s++;
+  }
+  
+  return len;
+}
+
+char *strcpy(char* dst,const char* src) {
+  assert(dst != NULL && src != NULL);
+
+  char* r = dst;
+
+  do {
+    *dst++ = *src++;
+  } while (*src != '\0');
+  
+  return r;
+}
+
+char* strncpy(char* dst, const char* src, size_t n) {
+  assert(dst != NULL && src != NULL);
+
+  char* r = dst;
+  size_t off = 0;
+
+  if (strlen(src) < n) { // 如果src长度小于len，就在后面补充\0
+    off = n - strlen(src);
+    n = strlen(src);
+  }
+
+  while (n--){
+    *dst++ = *src++;
+  }
+
+  while (off--) {
+    *dst++ = '\0';
+  }
+
+  return r;
+}
+
+char* strcat(char* dst, const char* src) {
+  assert(dst != NULL && src != NULL);
+
+  char* r = dst;
+
+  while (*dst != '\0') dst++;
+
+  do {
+    *dst++ = *src++;
+  } while (*src != '\0');
+  
+  return r;
+}
+
+int strcmp(const char* s1, const char* s2) {
+  while (*s1 == *s2) {
+    if (*s1 == '\0') return 0;
+    s1++;
+    s2++;
+  }
+
+  return *s1 - *s2;
+}
+
+int strncmp(const char* s1, const char* s2, size_t n) {
+  while (*s1 == *s2 && n--) {
+    if (*s1 == '\0') return 0;
+    s1++;
+    s2++;
+  }
+
+  return *s1 - *s2;
+}
+
+void* memset(void* v,int c,size_t n) {
+  char* r = (char*)v;
+  for (size_t i=0; i<n; i++) {
+    *(r+i) = (char) c;
+  }
+
+  return v;
+}
+
+void* memcpy(void* out, const void* in, size_t n) {
+  char* dst = (char*)out;
+  char* src = (char*)in;
+  size_t off = 0;
+
+  if (strlen(src) < n) { // 如果src长度小于len，就在后面补充\0
+    off = n - strlen(src);
+    n = strlen(src);
+  }
+
+  while (n--){
+    *dst++ = *src++;
+  }
+
+  while (off--) {
+    *dst++ = '\0';
+  }
+
+  return out;
+}
+
+int memcmp(const void* s1, const void* s2, size_t n){
+  char* s1_c = (char*) s1;
+  char* s2_c = (char*) s2;
+
+  while (*s1_c == *s2_c && n--) {
+    if (*s1_c == '\0') return 0;
+    s1_c++;
+    s2_c++;
+  }
+
+  return *s1_c - *s2_c;
+
+#endif
