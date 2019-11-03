1473fb9217a95b74c95cbf7ae89472bfd7ad221e
tracer-ics2019 <tracer@njuics.org>
2019-11-03 09:58:59 +0800

    >  compile 181220079 zoechou Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  09:58:59 up 23:53,  1 user,  load average: 0.48, 0.46, 0.35 2b80a9de75e37d33771c2a90ad956a3ea2e01cb2

--- nemu/src/isa/x86/exec/logic.c
+++ nemu/src/isa/x86/exec/logic.c
@@ -28,9 +28,12 @@ make_EHelper(and) {
 
 make_EHelper(xor) {
   printf("xor id_dest->val= %#x, id_src_val=%#x\n", id_dest->val, id_src->val);
-
+  printf("id_dest->reg:%#x\n", id_dest->reg);
+  printf("id_dest->addr:%#x\n", id_dest->addr);
+  
   rtl_xor(&s0, &id_dest->val, &id_src->val);
   operand_write(id_dest, &s0);
+  
   printf("s0: %#x\n", s0);
   printf("id_dest->reg:%#x\n", id_dest->reg);
   printf("id_dest->addr:%#x\n", id_dest->addr);
