665b31b20360e091c74e19e6efa7f6ea9f6c944c
181220079-Zhou Yawen <181220079@smail.nju.edu.cn>
2019-11-03 19:50:33 +0800

    complete _DEVREG_VIDEO_FBCTL

--- nexus-am/am/src/nemu-common/nemu-video.c
+++ nexus-am/am/src/nemu-common/nemu-video.c
@@ -1,15 +1,13 @@
 #include <am.h>
 #include <amdev.h>
 #include <nemu.h>
+#include <klib.h>
 
 #define SCREEN_PORT 0x100
 #define W 400
 #define H 300
 #define FPS 32
-
-void draw_sync();
-int screen_width();
-int screen_height();
+static uint32_t fb[W * H] = {};
 
 size_t __am_video_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
@@ -27,8 +25,15 @@ size_t __am_video_write(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_FBCTL: {
       _DEV_VIDEO_FBCTL_t *ctl = (_DEV_VIDEO_FBCTL_t *)buf;
+      int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
+      uint32_t *pixels = ctl->pixels;
+      int cp_bytes = sizeof(uint32_t) * min(w, W - x);
+      for (int i = 0; i < h && y + i < H; i ++) {
+        memcpy(&fb[(y + i) * W + x], pixels, cp_bytes);
+        pixels += w;
+      }
       if (ctl->sync) {
-        outl(SYNC_ADDR, 0);
+        //outl(SYNC_ADDR, 0);
       }
       return size;
     }
