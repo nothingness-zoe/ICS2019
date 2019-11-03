da1f594ad83b10caeea9478a62e7b3f6c12bbcb9
tracer-ics2019 <tracer@njuics.org>
2019-11-03 21:02:02 +0800

    >  compile 181220079 zoechou Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  21:02:02 up 1 day,  9:03,  1 user,  load average: 0.61, 0.34, 0.18 b635b6ee3c946506bb8caf6331704e5ff741fd0

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -32,7 +32,8 @@ static void vga_io_handler(uint32_t offset, int len, bool is_write) {
   // TODO: call `update_screen()` when writing to the sync register
   //TODO();
   if (is_write) {
-    update_screen();
+    for (; offset < len; offset++)
+      update_screen();
   }
 }
 
