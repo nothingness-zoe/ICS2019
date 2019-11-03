835049290fb0fbed926fc44806ee30d72b920f64
tracer-ics2019 <tracer@njuics.org>
2019-11-03 09:45:07 +0800

    >  compile 181220079 zoechou Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  09:45:07 up 23:39,  1 user,  load average: 0.42, 0.48, 0.31 41ab269e932718cfce8d82642093f372d35f835b

--- nemu/src/isa/x86/diff-test.c
+++ nemu/src/isa/x86/diff-test.c
@@ -4,25 +4,25 @@
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   if (  ref_r->eax != cpu.eax ||ref_r->ecx != cpu.ecx || ref_r->edx != cpu.edx 
   || ref_r->ebx != cpu.ebx || ref_r->esp != cpu.esp || ref_r->ebp != cpu.ebp
-  || ref_r->esi != cpu.esi || ref_r->edi != cpu.edi || ref_r->pc != pc) {
+  || ref_r->esi != cpu.esi || ref_r->edi != cpu.edi ) {
     if (ref_r->eax != cpu.eax)
-      printf("diff:\ncpu.eax=%#x,ref.eax=%#x", cpu.eax,ref_r->eax);
+      printf("diff:\ncpu.eax=%#x,ref.eax=%#x\n", cpu.eax,ref_r->eax);
     if (ref_r->ecx != cpu.ecx)
-      printf("diff:\ncpu.ecx=%#x,ref.ecx=%#x", cpu.ecx,ref_r->ecx);
+      printf("diff:\ncpu.ecx=%#x,ref.ecx=%#x\n", cpu.ecx,ref_r->ecx);
     if (ref_r->edx != cpu.edx)
-      printf("diff:\ncpu.edx=%#x,ref.edx=%#x", cpu.edx,ref_r->edx);
+      printf("diff:\ncpu.edx=%#x,ref.edx=%#x\n", cpu.edx,ref_r->edx);
     if (ref_r->ebx != cpu.ebx)
-      printf("diff:\ncpu.ebx=%#x,ref.ebx=%#x", cpu.ebx,ref_r->ebx);
+      printf("diff:\ncpu.ebx=%#x,ref.ebx=%#x\n", cpu.ebx,ref_r->ebx);
     if (ref_r->esp != cpu.esp)
-      printf("diff:\ncpu.esp=%#x,ref.esp=%#x", cpu.esp,ref_r->esp);
+      printf("diff:\ncpu.esp=%#x,ref.esp=%#x\n", cpu.esp,ref_r->esp);
     if (ref_r->ebp != cpu.ebp)
-      printf("diff:\ncpu.ebp=%#x,ref.ebp=%#x", cpu.ebp,ref_r->ebp);
+      printf("diff:\ncpu.ebp=%#x,ref.ebp=%#x\n", cpu.ebp,ref_r->ebp);
     if (ref_r->esi != cpu.esi)
-      printf("diff:\ncpu.esi=%#x,ref.esi=%#x", cpu.esi,ref_r->esi);
+      printf("diff:\ncpu.esi=%#x,ref.esi=%#x\n", cpu.esi,ref_r->esi);
     if (ref_r->edi != cpu.edi)
-      printf("diff:\ncpu.edi=%#x,ref.edi=%#x", cpu.edi,ref_r->eax);
-    if (ref_r->pc != pc)
-      printf("diff:\npc=%#x,ref.pc=%#x", pc,ref_r->pc);
+      printf("diff:\ncpu.edi=%#x,ref.edi=%#x\n", cpu.edi,ref_r->eax);
+    /*if (ref_r->pc != pc)
+      printf("diff:\npc=%#x,ref.pc=%#x\n", pc,ref_r->pc);*/
     return false;
   }
     
