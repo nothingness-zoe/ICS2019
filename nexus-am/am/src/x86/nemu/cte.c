542e591b78af111813a6e11e062773bfaf4f33fb
181220079-Zhou Yawen <181220079@smail.nju.edu.cn>
2019-11-18 00:16:49 +0800

    complete _EVENT_YIELD

--- nexus-am/am/src/x86/nemu/cte.c
+++ nexus-am/am/src/x86/nemu/cte.c
@@ -10,19 +10,21 @@ void __am_vecnull();
 
 _Context* __am_irq_handle(_Context *c) {
   _Context *next = c;
-  printf("eax: %#x\n", c->eax);
-  printf("ecx: %#x\n", c->ecx);
-  printf("edx: %#x\n", c->edx);
-  printf("ebx: %#x\n", c->ebx);
-  printf("esp: %#x\n", c->esp);
-  printf("ebp: %#x\n", c->ebp);
-  printf("esi: %#x\n", c->esi);
-  printf("edi: %#x\n", c->edi);
-  printf("eip: %#x\n", c->eip);
-  printf("eflags: %#x\n", c->eflags);
+  // printf("eax: %#x\n", c->eax);
+  // printf("ecx: %#x\n", c->ecx);
+  // printf("edx: %#x\n", c->edx);
+  // printf("ebx: %#x\n", c->ebx);
+  // printf("esp: %#x\n", c->esp);
+  // printf("ebp: %#x\n", c->ebp);
+  // printf("esi: %#x\n", c->esi);
+  // printf("edi: %#x\n", c->edi);
+  // printf("eip: %#x\n", c->eip);
+  // printf("eflags: %#x\n", c->eflags);
   if (user_handler) {
     _Event ev = {0};
     switch (c->irq) {
+      case 0x81: ev.event = _EVENT_YIELD; break;
+      case 0x80: ev.event = _EVENT_SYSCALL; break;
       default: ev.event = _EVENT_ERROR; break;
     }
 
