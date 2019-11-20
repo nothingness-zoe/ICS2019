077f15e33dd2e9021fff6baac7a99f628506cf7a
tracer-ics2019 <tracer@njuics.org>
2019-11-21 01:40:22 +0800

    >  run 181220079 zoechou Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  01:40:22 up 12:48,  1 user,  load average: 0.50, 0.32, 0.22 b5663627e2e6f07a8bb2a81a2ca4eb8df6fb9b15

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -10,7 +10,7 @@ _Context* do_syscall(_Context *c) {
 
   switch (a[0]) {
     case SYS_exit: _halt(a[1]); break;
-    case SYS_yield: _yield(); c->GRPX = 0; break;
+    case SYS_yield: _yield(); c->GPRx = 0; break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
