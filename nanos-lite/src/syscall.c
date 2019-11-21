6ed7638ef8a32fa5f938bf6c50052a6b7ff43c04
tracer-ics2019 <tracer@njuics.org>
2019-11-21 10:19:01 +0800

    >  run 181220079 zoechou Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  10:19:01 up 15:52,  1 user,  load average: 0.82, 0.83, 0.59 30f9edcd8f5e39efb0389d18fc02692a5de749

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -2,7 +2,7 @@
 #include "syscall.h"
 
 uintptr_t sys_write(uintptr_t fd, void* buf, uintptr_t count) {
-  _putc('s'); _putc('\n');
+  _putc('w'); _putc('\n');
   uintptr_t count_write = 0;
   if (fd == 1 || fd == 2) {
     char * out = (char *)buf;
@@ -29,7 +29,7 @@ _Context* do_syscall(_Context *c) {
   switch (a[0]) {
     case SYS_exit: _halt(a[1]); break;
     case SYS_yield: _yield(); c->GPRx = 0; break;
-    case SYS_write: c->GPRx = sys_write(a[1], (void*)a[2], a[3]); break;
+    case SYS_write: _putc('c'); c->GPRx = sys_write(a[1], (void*)a[2], a[3]); break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
