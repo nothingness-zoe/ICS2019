4708a5fb97271d829b0315b2428f40f0f7a15162
tracer-ics2019 <tracer@njuics.org>
2019-11-24 01:31:52 +0800

    >  run 181220079 zoechou Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  01:31:52 up  5:09,  1 user,  load average: 0.32, 0.29, 0.12 6880c64349d32b2f6e5f34054727a4e8ccc1cd68

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -59,10 +59,11 @@ int _open(const char *path, int flags, mode_t mode) {
 }
 
 int _write(int fd, void *buf, size_t count) {
-  return _syscall_(SYS_write, fd, buf, count);
+  return _syscall_(SYS_write, fd, (intptr_t)buf, count);
 }
 
 void *_sbrk(intptr_t increment) {
+  
   return (void *)-1;
 }
 
