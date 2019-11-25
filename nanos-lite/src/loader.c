4fddc64b0b57d35606ab460a67fce5721e21ea20
181220079-Zhou Yawen <181220079@smail.nju.edu.cn>
2019-11-25 15:24:40 +0800

    debug for loader

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -8,17 +8,29 @@
 # define Elf_Ehdr Elf32_Ehdr
 # define Elf_Phdr Elf32_Phdr
 #endif
-# define DEFAULT_ENTRY 0x3000000
+// # define DEFAULT_ENTRY 0x3000000
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
   // TODO();
   // printf("remdisk_size: %x\n", get_ramdisk_size());
   //uintptr_t DEFAULT_ENTRY = 0x3000000;
-  ramdisk_read((void *)DEFAULT_ENTRY, 0x1000, get_ramdisk_size()-0x1000);
+  // ramdisk_read((void *)DEFAULT_ENTRY, 0x1000, get_ramdisk_size()-0x1000);
   // ramdisk_read((void *)DEFAULT_ENTRY, 0x1000, 0x28f3-0x1000);
   // printf("entry: %x\n", (uintptr_t)DEFAULT_ENTRY);
   // printf("success\n");
-  return (uintptr_t)DEFAULT_ENTRY;
+  // return (uintptr_t)DEFAULT_ENTRY;
+  Elf_Ehdr ehdr;
+  ramdisk_read(&ehdr, 0, sizeof(ehdr));
+
+  for (int i=0; i < ehdr.e_phnum; i++) {
+    Elf_Phdr phdr;
+    ramdisk_read(&phdr, ehdr.e_phoff + i*ehdr.e_phentsize, ehdr.e_phentsize);
+    if (phdr.p_type == PT_LOAD) {
+      ramdisk_read((void*)phdr.p_vaddr, phdr.p_offset, phdr.p_filesz);
+      memset((void*)(phdr.p_vaddr + phdr.p_filesz), 0, phdr.p_memsz - phdr.p_filesz);
+    }
+    return ehdr.e_entry;
+  }
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
