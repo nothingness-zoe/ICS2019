e8aaf3f6106c585c8d48427c65c8208e63571ac0
181220079-Zhou Yawen <181220079@smail.nju.edu.cn>
2019-12-01 16:34:04 +0800

    debug for fs.c

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -46,7 +46,6 @@ int fs_open (const char *pathname, int flags, int mode){
   for (int i=0; i < NR_FILES; i++) {
     if (strcmp(file_table[i].name, pathname) == 0) {
       printf("%s\n", pathname);
-      file_table[i].open_offset = 0;
       return i;
     }
   }
