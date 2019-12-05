2d7f024166bc582bb11daefabf49d6d16994112a
tracer-ics2019 <tracer@njuics.org>
2019-12-05 16:51:44 +0800

    >  run 181220079 zoechou Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  16:51:44 up 1 day, 15:46,  1 user,  load average: 0.12, 0.22, 0.35 5ecb27502cec98aac87b9c79604d2db5a8c50e66

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -54,7 +54,7 @@ size_t fb_write(const void *buf, size_t offset, size_t len) {
 
 size_t fbsync_write(const void *buf, size_t offset, size_t len) {
   draw_sync();
-  printf("success\n");
+  // printf("success\n");
   return len;
 }
 
