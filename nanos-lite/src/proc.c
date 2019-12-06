1f23183eacd1fd8743b47ca97d0645940ef30f34
tracer-ics2019 <tracer@njuics.org>
2019-12-06 16:28:50 +0800

    >  run 181220079 zoechou Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  16:28:50 up 2 days, 19 min,  1 user,  load average: 1.12, 1.02, 0.75 d0739778297c5ccff8167511b8b92eee5c7b2203

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -25,7 +25,7 @@ void init_proc() {
   Log("Initializing processes...");
 
   // load program here
-  naive_uload(NULL, "/bin/init");
+  naive_uload(NULL, "/bin/pal");
   // Log("Finish initializing processes...");
 }
 
