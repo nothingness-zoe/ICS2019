b70d7f71bb7e30158cb4e2e79341773096503412
tracer-ics2019 <tracer@njuics.org>
2019-12-06 15:45:52 +0800

    >  compile 181220079 zoechou Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  15:45:52 up 1 day, 23:36,  1 user,  load average: 1.68, 0.67, 0.47 4ebfee788956267e9ba3f4da16002893145463c0

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -30,7 +30,7 @@ static Finfo file_table[] __attribute__((used)) = {
   {"stdout", 0, 0, 0, invalid_read, serial_write},
   {"stderr", 0, 0, 0, invalid_read, serial_write},
   {"/dev/events", 0, 0, 0, events_read, invalid_write},
-  {"/dev/fb", 0, 0, 0, NULL, fb_write},
+  {"/dev/fb", 0, 0, 0, invalid_read, fb_write},
   {"/dev/fbsync", 0, 0, 0, events_read, fbsync_write},
   {"/proc/dispinfo", 128, 0, 0, dispinfo_read, invalid_write},
   {"/dev/tty", 0, 0, 0, invalid_read, serial_write},
