ab8529ec04c1f12c004ac96ffc067056f86b5542
tracer-ics2019 <tracer@njuics.org>
2019-12-06 16:27:35 +0800

    >  run 181220079 zoechou Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  16:27:35 up 2 days, 18 min,  1 user,  load average: 1.37, 1.01, 0.73 31e7d6e67259a15d178ca4056da71be4ff0f4fd4

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -81,7 +81,7 @@ size_t fs_read(int fd, void * buf, size_t len) {
     // printf("open_offset:%d  len:%d\n", file_table[fd].open_offset,len);
     size_t num = file_table[fd].read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
     file_table[fd].open_offset += len;
-    printf("open_offset:%d  len:%d num:%d\n", file_table[fd].open_offset,len, num);
+    //printf("open_offset:%d  len:%d num:%d\n", file_table[fd].open_offset,len, num);
     return num;
   }
 }
