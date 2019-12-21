d29ede5f94e952188ef7171e587d2537060c1316
tracer-ics2019 <tracer@njuics.org>
2019-12-21 12:50:42 +0800

    >  run 181220079 zoechou Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  12:50:42 up 10:04,  1 user,  load average: 0.60, 0.37, 0.26 abdd6c74aa067601c7df5f91ded6c2a8a9d4c4e

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -16,20 +16,24 @@ void free_page(void *p) {
 
 /* The brk() system call handler. */
 int mm_brk(uintptr_t brk, intptr_t increment) {
+  if (current->max_brk == 0) {
+    current->max_brk = (brk & 0xfff) ? ((brk & ~0xfff)+PGSIZE) : brk;
+    return 0;
+  }
   if (current->max_brk < brk) {
-    printf("mm_brk work\nbrk: %d  max_brk: %d\n", brk, current->max_brk);
+    // printf("mm_brk work\nbrk: %d  max_brk: %d\n", brk, current->max_brk);
     uintptr_t va;
     void* pa;
     uintptr_t start;
     if ((current->max_brk & ~0xfff) == current->max_brk) start = current->max_brk;
     else start = (current->max_brk & ~0xfff) + PGSIZE;
     for (va = start; va < brk; va += PGSIZE) {
-      printf("one time\n");
+      // printf("one time\n");
       pa = new_page(1);
       _map(&current->as, (void*)va, pa, 1);
     }
     current->max_brk = brk;
-    printf("mm_brk success\n");
+    // printf("mm_brk success\n");
   }
   return 0;
 
