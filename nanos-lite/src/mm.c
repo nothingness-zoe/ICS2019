03fc2cf8ab7e3f3f4db1a283f66db7d865275d72
tracer-ics2019 <tracer@njuics.org>
2019-12-22 18:07:57 +0800

    >  run 181220079 zoechou Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  18:07:57 up 21:19,  1 user,  load average: 0.37, 0.25, 0.14 7ef3aa2c8ebdf642e02639486a8244739c59c79e

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -15,31 +15,34 @@ void free_page(void *p) {
 }
 
 extern uintptr_t heapstart;
+extern int nr_page;
 /* The brk() system call handler. */
 int mm_brk(uintptr_t brk, intptr_t increment) {
   if (current->max_brk == 0) {
     printf("mm_brk work\nbrk: %p  increment: %p max_brk: %p\n", brk, increment, current->max_brk);
     printf("current->as: %p\n", current->as);
     // current->max_brk = (brk & 0xfff) ? ((brk & ~0xfff)+PGSIZE) : brk;
-    // current->max_brk = brk;
-    current->max_brk = heapstart;
+    current->max_brk = brk;
+    // current->max_brk = heapstart;
+    // current->max_brk = 0x40000000+nr_page*PGSIZE;
     // current->max_brk = (uintptr_t)_heap.start;
-    if (current->max_brk < brk + increment) {
-      printf("mm_brk work\nbrk: %p  max_brk: %p\n", brk, current->max_brk);
-      uintptr_t va;
-      void* pa;
-      uintptr_t start;
-      if ((current->max_brk & ~0xfff) == current->max_brk) start = current->max_brk;
-      else start = (current->max_brk & ~0xfff) + PGSIZE;
-      for (va = start; va < brk + increment; va += PGSIZE) {
-        // printf("one time\n");
-        pa = new_page(1);
-        _map(&current->as, (void*)va, pa, 1);
-      }
-      current->max_brk = brk+increment;
-      printf("mm_brk success\n");
-    }
   }
+  if (current->max_brk < brk + increment) {
+    printf("mm_brk work\nbrk: %p  max_brk: %p\n", brk, current->max_brk);
+    uintptr_t va;
+    void* pa;
+    uintptr_t start;
+    if ((current->max_brk & ~0xfff) == current->max_brk) start = current->max_brk;
+    else start = (current->max_brk & ~0xfff) + PGSIZE;
+    for (va = start; va < brk + increment; va += PGSIZE) {
+      // printf("one time\n");
+      pa = new_page(1);
+      _map(&current->as, (void*)va, pa, 1);
+    }
+    current->max_brk = brk+increment;
+    printf("mm_brk success\n");
+  }
+
   return 0;
 
 }
